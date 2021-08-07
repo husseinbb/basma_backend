5d7dcf662c8abdab1e6ec63158b3f8116c22ab5b
Hussein Barakat <h.barakat@totersapp.com>
2021-08-07 13:15:32 +0300

    logout route

--- routes/api.php
+++ routes/api.php
@@ -21,9 +21,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-Route::post('register', [RegisterController::class, 'register']);
-Route::post('login', [LoginController::class, 'authenticate']);
+Route::post('register', [App\Http\Controllers\Auth\RegisterController::class, 'register']);
+Route::post('login', [App\Http\Controllers\Auth\LoginController::class, 'authenticate']);
 
 Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::get('logout', [App\Http\Controllers\Auth\LogoutController::class, 'logout']);
 
 });
