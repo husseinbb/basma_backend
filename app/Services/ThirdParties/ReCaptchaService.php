3753b3e1dfbdfa7d210070bc583c5a30a2023a80
Hussein Barakat <h.barakat@totersapp.com>
2021-08-11 23:55:12 +0300

    add keys

--- app/Services/ThirdParties/ReCaptchaService.php
+++ app/Services/ThirdParties/ReCaptchaService.php
@@ -9,6 +9,7 @@ class ReCaptchaService
 {
     public function verifyReCaptcha($data)
     {
+        $data['secret'] = config('keys.recaptcha.secret_key');
         $client = new GuzzleHttp\Client();
         $res = $client->post('https://www.google.com/recaptcha/api/siteverify', $data);
         return json_decode($res->getBody());
